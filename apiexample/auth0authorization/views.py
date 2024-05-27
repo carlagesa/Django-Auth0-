229ede565554f6f56abbc48afc263e05d3f80147
carlton <carltonagesa@gmail.com>
2024-05-27 21:38:38 +0300

    Protect API Endpoint

--- apiexample/auth0authorization/views.py
+++ apiexample/auth0authorization/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from functools import wraps
 import jwt
 from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 
 def get_token_auth_header(request):
     """Obtains the Access Token from the Authorization Header
@@ -34,3 +36,13 @@ def requires_scope(required_scope):
             return response
         return decorated
     return require_scope
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def public(request):
+    return JsonResponse({'message': 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'})
+
+
+@api_view(['GET'])
+def private(request):
+    return JsonResponse({'message': 'Hello from a private endpoint! You need to be authenticated to see this.'})
