e188fa808827c88b51b3bbf3e385d1c2e1502fff
carlton <carltonagesa@gmail.com>
2024-05-27 21:30:02 +0300

    Function to fetch the JWKS from Auth0 acc

--- apiexample/auth0authorization/utils.py
+++ apiexample/auth0authorization/utils.py
@@ -1,7 +1,24 @@
 # auth0authorization/utils.py
 from django.contrib.auth import authenticate
+import json
+import jwt
+import requests
 
 def jwt_get_username_from_payload_handler(payload):
     username = payload.get('sub').replace('|', '.')
     authenticate(remote_user=username)
     return username
+
+def jwt_decode_token(token):
+    header = jwt.get_unverified_header(token)
+    jwks = requests.get('https://{}/.well-known/jwks.json'.format('{yourDomain}')).json()
+    public_key = None
+    for jwk in jwks['keys']:
+        if jwk['kid'] == header['kid']:
+            public_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
+
+    if public_key is None:
+        raise Exception('Public key not found.')
+
+    issuer = 'https://{}/'.format('{yourDomain}')
+    return jwt.decode(token, public_key, audience='{yourApiIdentifier}', issuer=issuer, algorithms=['RS256'])
